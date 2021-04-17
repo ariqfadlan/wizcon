644f07bef1e8ee106a70b8c4a035cd0ebe1123af
Ariq Fadlan <ariqfadlan@hotmail.co.id>
2021-04-17 16:22:19 +0700

    remove unsupported command for turnable white

--- wizcon/wizcon.py
+++ wizcon/wizcon.py
@@ -52,14 +52,7 @@ def parse_args(args):
         formatter_class=argparse.RawDescriptionHelpFormatter,
         epilog=textwrap.dedent('''\
         Scene Table
-        1: "Ocean"
-        2: "Romance"
-        3: "Sunset"
-        4: "Party"
-        5: "Fireplace"
         6: "Cozy"
-        7: "Forest"
-        8: "Pastel Colors"
         9: "Wake up"
         10: "Bedtime"
         11: "Warm White"
@@ -68,18 +61,7 @@ def parse_args(args):
         14: "Night light"
         15: "Focus"
         16: "Relax"
-        17: "True colors"
         18: "TV time"
-        19: "Plantgrowth"
-        20: "Spring"
-        21: "Summer"
-        22: "Fall"
-        23: "Deepdive"
-        24: "Jungle"
-        25: "Mojito"
-        26: "Club"
-        27: "Christmas"
-        28: "Halloween"
         29: "Candlelight"
         30: "Golden white"
         31: "Pulse"
@@ -107,7 +89,7 @@ def parse_args(args):
         '''))
     parser.add_argument('IP', type=str, help='IP address of smart bulb')
     parser.add_argument('COMMAND', type=str.upper, choices=['ON', 'OFF', 'SWITCH'], help='Command sent to the smart bulb')
-    parser.add_argument('-si', '--scene_id', type=int, choices=range(1,33), metavar='{1-32}', help='Set scene of smart bulb using scene ID')
+    parser.add_argument('-si', '--scene_id', type=int, choices=[6, *range(9,17), 18, *range(29,33)], metavar='6 | {9,16} | 18 | {29-32}', help='Set scene of smart bulb using scene ID')
     #parser.add_argument('-sn', '--scene_name', type=str, help='Set scene of smart bulb using scene name')
     #parser.add_argument('-c', '--color', type=str, help='Set color of smart bulb')
     parser.add_argument('-b', '--brightness', type=int, choices=range(0, 256), metavar='{0-255}', help='Set brightness of smart bulb')
